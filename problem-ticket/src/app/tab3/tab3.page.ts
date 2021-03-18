e0ab931f1105cc56c7c60fb80dcf7b1f540e2d1f
Gregorio Sonit <gregorio.sonit@evsu.edu.ph>
2021-03-18 11:15:36 +0800

    Local Storage Sample

--- problem-ticket/src/app/tab3/tab3.page.ts
+++ problem-ticket/src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ContactServiceService } from '../services/contact-service.service';
+
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  contName = "";
+  contNumber = ""
+  contacts: any = [];
 
-  constructor() {}
+
+  constructor(public contactService: ContactServiceService) { }
+
+  async ngOnInit() {
+    this.contactService.getData().then(data => {
+        this.contacts = data;
+    });
+  }
+
+  saveC() {
+    this.contactService.saveContact({name: this.contName, number: this.contNumber}).then(data => {
+      this.contacts = data;
+      this.clearField()
+    });
+  }
+
+  clearField() {
+    this.contName = "";
+    this.contNumber = "";
+  }
 
 }
