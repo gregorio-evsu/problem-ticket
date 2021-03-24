04eb3f62c8be05273d9adfef0a5332eeec029187
Gregorio Sonit <gregorio.sonit@evsu.edu.ph>
2021-03-24 10:47:33 +0800

    Adding and deleting in local storage

--- problem-ticket/src/app/services/contact-service.service.ts
+++ problem-ticket/src/app/services/contact-service.service.ts
@@ -12,15 +12,21 @@ export class ContactServiceService {
   DATA_STORAGE = 'data';
   constructor() { }
 
+  /**
+   * Save contact into local storage 
+   * get first data and iterate save to temporary variable and insert new data.
+   * @param param 
+   * @returns 
+   */
   saveContact(param: any): Promise<any> {
     return new Promise((resolve, reject) => {
       this.getData().then(data => {
         let tempContact = [];
-        for(let i = 0; i < data.length; i++) {
+        for (let i = 0; i < data.length; i++) {
           tempContact.push(data[i]);
         }
         tempContact.push(param);
-        Storage.set({key: this.DATA_STORAGE, value: JSON.stringify(tempContact)});
+        Storage.set({ key: this.DATA_STORAGE, value: JSON.stringify(tempContact) });
         resolve(tempContact);
       })
     });
@@ -28,6 +34,26 @@ export class ContactServiceService {
 
 
 
+  /*
+   * Removing contact from the local storage.
+   * Remove first existing storage and set new data.
+  */
+  removeContact(index): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.getData().then(data => {
+        data.splice(index, 1);
+        console.log(data);
+        Storage.remove({ key: this.DATA_STORAGE })
+        Storage.set({ key: this.DATA_STORAGE, value: JSON.stringify(data) });
+        resolve(data);
+      })
+    })
+  }
+
+  /**
+   * Retrieve data from local storage and convert to JSON
+   * @returns local storage (this.DATA_STORAGE)
+   */
   public async getData() {
     const cont = await Storage.get({ key: this.DATA_STORAGE });
     return JSON.parse(cont.value) || [];
--- problem-ticket/src/app/tab3/tab3.page.ts
+++ problem-ticket/src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ContactServiceService } from '../services/contact-service.service';
+import { ToastController } from '@ionic/angular';
 
 
 @Component({
@@ -13,20 +14,53 @@ export class Tab3Page {
   contacts: any = [];
 
 
-  constructor(public contactService: ContactServiceService) { }
+  constructor(public contactService: ContactServiceService, public toast: ToastController) { }
 
   async ngOnInit() {
     this.contactService.getData().then(data => {
-        this.contacts = data;
+      this.contacts = data;
     });
   }
 
   saveC() {
-    this.contactService.saveContact({name: this.contName, number: this.contNumber}).then(data => {
+    this.contactService.saveContact({ name: this.contName, number: this.contNumber }).then(data => {
       this.contacts = data;
       this.clearField()
     });
   }
+
+  /**
+   * show toast message to confirm deletion
+   * call delete function from the contact-service.
+   * @param index 
+   */
+  async deleteContact(index) {
+    const toasts = await this.toast.create({
+      message: 'Delete Contact',
+      position: "top",
+      buttons: [
+        {
+          icon: "trash",
+          side: 'end',
+          text: "Yes",
+          handler: () => {
+            console.log(index);
+            this.contactService.removeContact(index).then(data => {
+              this.contacts = data;
+            })
+          }
+        }, {
+          side: "start",
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('cancel deletion')
+          }
+        }]
+    })
+    toasts.present();
+  }
+
 
   clearField() {
     this.contName = "";
